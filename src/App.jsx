801604aa3a20cfb30f116b08d6f19ebef8e344aa
FELIPE ASSIS DE JESUS <45512256+lipeA@users.noreply.github.com>
2024-02-24 04:03:49 -0300

    primeiro commit do projeto

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import Converter from './components/converter';
+
+
+function App() {
+
+
+  return (
+      <div className=' w-screen h-screen flex justify-center items-center'>
+        <div className=' border-2 border-slate-950 w-1/3 h-2/5 rounded-md bg-slate-300 ' >
+        <Converter/>
+        </div>
+      </div>
+  );
+}
+
+export default App
---
+++ src/components/converter.jsx
@@ -0,0 +1,27 @@
+
+
+export default function Converter() {
+    return (
+        <>
+            <div className="flex flex-col justify-center items-center mt-8 ">
+                <h1 className="" >Conversor de moedas</h1>
+                <input type="number" placeholder="Digite o valor" />
+                <span>Selecione uma moeda</span>
+                <select name="" id="" placeholder="BR">
+
+                </select>
+                <span>para</span>
+                <select name="" id="" placeholder="USD">
+
+                </select>
+                <span>valor convertido</span>
+
+
+            </div>
+
+
+
+        </>
+
+    );
+}
